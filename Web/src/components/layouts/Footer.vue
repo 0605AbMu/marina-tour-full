f0ffcaa0e05e050fb95887394755b6d55055aaa1
0605abmu <0605AbMu@gmail.com>
2024-08-22 20:43:10 +0500

    feat: add company informations

--- Web/src/components/layouts/Footer.vue
+++ Web/src/components/layouts/Footer.vue
@@ -3,14 +3,23 @@
   <div class="w-full p-10 bg-[#1A080A] flex flex-col justify-center items-center gap-y-5" id="blog">
     <div class="flex flex-row w-full gap-y-10 m-10 mb-2 justify-center flex-wrap gap-x-56">
       <div>
-        <p class="text-2xl text-white">{{ $t('contacts') }}</p>
+        <p class="text-2xl text-white">{{ $t('company') }}</p>
+        <p class="text-gray-400 my-5">
+          <i class="bi bi-geo-alt-fill"></i>&nbsp;TOSHKENT SHAHAR, UCHTEPA TUMAN,11-MAVZE,<br>ZARGARLIK 48-UY 50-XONADON
+        </p>
         <p class="text-gray-400 my-5">
           <i class="bi bi-telephone-fill"></i>&nbsp;<a href="tel:+998970111482">+998970111482</a>
         </p>
         <p class="text-gray-400 my-5">
           <i class="bi bi-envelope-fill"></i>&nbsp;<a href="mailto:marinatraveluz@gmail.com">
-            marinatraveluz@gmail.com</a
-          >
+          marinatraveluz@gmail.com</a
+        >
+        </p>
+        <p class="text-gray-400 my-5">
+          <i class="bi bi-calendar-date-fill"></i>&nbsp;9.00 dan - 20.00 gacha 24/7
+        </p>
+        <p class="text-gray-400 my-5">
+          Rahbar: <i>Y.X.Yangiboyev</i>&nbsp;
         </p>
         <!-- <div class="bg-white">
           <a
@@ -55,7 +64,7 @@
 
     <div class="divider"></div>
     <p class="text-gray-400">
-      Copyright © {{ new Date().getFullYear() }} Marina Travel, {{ $t('allRightsReserved')}}.
+      Copyright © {{ new Date().getFullYear() }} Marina Travel, {{ $t('allRightsReserved') }}.
     </p>
   </div>
 </template>
